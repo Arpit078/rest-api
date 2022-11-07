172ffd76e27506b6de974edaead686117aa2e6ff
Arpit078 <verma.arpit078@.com>
2022-11-08 01:30:32 +0530

    dad

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const app = express()
-const PORT = 5000
+const axios = require("axios")
+const PORT = 5000||process.env.PORT
 
 
 app.get("/",(req,res)=>{
     res.json("hello")
 })
 
+app.get('/comic',(req,res)=>{
+    const random_int = Math.floor(Math.random() * 2500);
+    axios.get(`https://xkcd.com/${random_int}/`)
+    .then((response)=>{
+        const html = response.data
+        const $ = cheerio.load(html)//$(".itemListElement")
+        const comic_img = $("#middleContainer")
+        const url = comic_img.find("a")
+        .next()
+        .next()
+        .text()
+        res.json(url)
+      
+  
+  
+    }).catch((err)=> console.log(err))
+  })
 
 app.listen(PORT,(req,res)=>{
     console.log("server started")
